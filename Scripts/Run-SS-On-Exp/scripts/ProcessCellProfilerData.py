81eed1d79f32b92133d7f0e5155784ea331c37a5
Atiyeh Ahmadi <a94ahmad@uwaterloo.ca>
2022-09-10 08:12:28 -0400

    files have been removed (because I changed the name of the directory since it wasn't a good representative of the whole capability of that)

--- Scripts/Run-SS-On-Exp/scripts/ProcessCellProfilerData.py
+++
@@ -1,9 +0,0 @@
-class Dict2Class(object):
-    """
-        goal:  Change Dictionary Into Class
-
-    """
-
-    def __init__(self, my_dict):
-        for key in my_dict:
-            setattr(self, key, my_dict[key])
