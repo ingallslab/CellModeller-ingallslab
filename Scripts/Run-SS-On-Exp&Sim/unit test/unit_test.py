258340ec0ee8034bdd731b40fe8651521e1cb062
Atiyeh Ahmadi <a94ahmad@uwaterloo.ca>
2022-09-17 11:03:45 -0400

    location of some parameters have been changed in the code
    note: the point is if a parameter should be defined with a default value, we should define it at the end of the the input arguments.

--- Scripts/Run-SS-On-Exp&Sim/unit test/unit_test.py
+++ Scripts/Run-SS-On-Exp&Sim/unit test/unit_test.py
@@ -10,33 +10,9 @@ if __name__ == '__main__':
     #  cellModeller output pickle files
     input_directory = '../input files/CellModeller_Sample'
     summary_statistic_method_list = ["Aspect Ratio", "Anisotropy", 'Density', 'dist_vs_growth_rate']
-    # mode: local (calculate for each micro colony) or global(calculate over time lapse without considering 
-    # micro-colonies)
-    mode1 = "local"
-    mode2 = 'global'
-    # unit: um
-    min_size_of_micro_colony = 2
-    max_distance_between_cells = 3.4
-    # convert micro meter to pixel
-    um_pixel_ratio = 0.144
     dt = 0.05  # interval time: 3 min
-    # mean_summary_statistic_report = {summary statistic name: mean value, ....}
-    # local summary statistics
-    local_mean_summary_statistic_report = data_analysis(input_directory, summary_statistic_method_list, mode1,
-                                                        dt, max_distance_between_cells, um_pixel_ratio,
-                                                        min_size_of_micro_colony)
-    # global summary statistics
-    global_mean_summary_statistic_report = data_analysis(input_directory, summary_statistic_method_list, mode2,
-                                                         dt, max_distance_between_cells, um_pixel_ratio)
-    # mean value of each summary statistic
-    print(local_mean_summary_statistic_report)
-    print(global_mean_summary_statistic_report)
-'''
 
-if __name__ == '__main__':
-    # cellprofiler output pickle files
-    input_directory = '../input files/CellProfiler_Sample'
-    summary_statistic_method_list = ["Aspect Ratio", "Anisotropy", 'Density', 'dist_vs_growth_rate']
+    # optional arguments
     # mode: local (calculate for each micro colony) or global(calculate over time lapse without considering
     # micro-colonies)
     mode1 = "local"
@@ -46,15 +22,45 @@ if __name__ == '__main__':
     max_distance_between_cells = 3.4
     # convert micro meter to pixel
     um_pixel_ratio = 0.144
-    dt = 0.05  # interval time: 3 min
+
     # mean_summary_statistic_report = {summary statistic name: mean value, ....}
     # local summary statistics
-    local_mean_summary_statistic_report = data_analysis(input_directory, summary_statistic_method_list, mode1,
-                                                        dt, max_distance_between_cells, um_pixel_ratio,
-                                                        min_size_of_micro_colony)
+    local_mean_summary_statistic_report = data_analysis(input_directory, summary_statistic_method_list, mode1, dt,
+                                                        mode1, max_distance_between_cells,
+                                                        um_pixel_ratio, min_size_of_micro_colony)
     # global summary statistics
-    global_mean_summary_statistic_report = data_analysis(input_directory, summary_statistic_method_list, mode2,
-                                                         dt, max_distance_between_cells, um_pixel_ratio)
+    global_mean_summary_statistic_report = data_analysis(input_directory, summary_statistic_method_list, dt, mode2,
+                                                         max_distance_between_cells, um_pixel_ratio)
+    # mean value of each summary statistic
+    print(local_mean_summary_statistic_report)
+    print(global_mean_summary_statistic_report)
+'''
+
+if __name__ == '__main__':
+    # cellprofiler output pickle files
+    input_directory = '../input files/CellProfiler_Sample'
+    summary_statistic_method_list = ["Aspect Ratio", "Anisotropy", 'Density', 'dist_vs_growth_rate']
+    dt = 0.05  # interval time: 3 min
+
+    # optional arguments
+    # mode: local (calculate for each micro colony) or global(calculate over time lapse without considering
+    # micro-colonies)
+    mode1 = "local"
+    mode2 = 'global'
+    # unit: um
+    min_size_of_micro_colony = 2
+    max_distance_between_cells = 3.4
+    # convert micro meter to pixel
+    um_pixel_ratio = 0.144
+
+    # mean_summary_statistic_report = {summary statistic name: mean value, ....}
+    # local summary statistics
+    local_mean_summary_statistic_report = data_analysis(input_directory, summary_statistic_method_list, mode1, dt,
+                                                        mode1, max_distance_between_cells,
+                                                        um_pixel_ratio, min_size_of_micro_colony)
+    # global summary statistics
+    global_mean_summary_statistic_report = data_analysis(input_directory, summary_statistic_method_list, dt, mode2,
+                                                         max_distance_between_cells, um_pixel_ratio)
     # mean value of each summary statistic
     print(local_mean_summary_statistic_report)
     print(global_mean_summary_statistic_report)
