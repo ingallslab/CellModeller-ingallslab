e85529049b182e0f4064ef6e9f7cb18ebb49794c
cheekolegend <aaronyip.ca@gmail.com>
2022-11-17 10:21:43 -0500

    Update test script

--- Scripts/summaryStatistics/biophysics/test_summary_stats.py
+++ Scripts/summaryStatistics/biophysics/test_summary_stats.py
@@ -20,6 +20,6 @@ print("aspect_ratio = ", aspect_ratio)
 density_parameter = density_calculation.get_density_parameter(cells)
 print("density_parameter = ", density_parameter)
 
-dt = 0.05
-growth_rate_parameter = growth_rate_analysis.get_growth_rate_vs_position_parameter(cells, dt)
+dt = 0.02
+growth_rate_parameter = growth_rate_analysis.get_growth_parameter(cells, dt, show_plots=False)
 print("growth_rate_parameter = ", growth_rate_parameter)
