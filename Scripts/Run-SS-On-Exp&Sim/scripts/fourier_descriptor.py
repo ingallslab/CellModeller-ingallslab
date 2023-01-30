7e9955a4d84601f8a79f8b91a1e2296fa8e3ef9c
Atiyeh Ahmadi <a94ahmad@uwaterloo.ca>
2023-01-30 14:00:36 -0500

    Calculation of fourier_desccriptor
    The goal is to capture colony border jaggedness. I define a parameter "skip" in this calculation to optimize the level of accuracy/complexity of jaggedness. "skip" is based on sampling from boundary's points.

---
+++ Scripts/Run-SS-On-Exp&Sim/scripts/fourier_descriptor.py
@@ -0,0 +1,95 @@
+import matplotlib.pyplot as plt
+import image_processing
+import BacteriaFeatures
+import numpy as np
+
+
+def spatial_moment(x_coordinates, y_coordinates, p, q):
+
+    sum_y = 0
+    m_pq = 0
+
+    for y in y_coordinates:
+        sum_y += (y ** q)
+
+    for x in x_coordinates:
+        m_pq += ((x ** p) * sum_y)
+
+    return m_pq
+
+
+def center_coordinate(x_coordinates, y_coordinates):
+
+    m_00 = spatial_moment(x_coordinates, y_coordinates, p=0, q=0)
+    m_10 = spatial_moment(x_coordinates, y_coordinates, p=1, q=0)
+    m_01 = spatial_moment(x_coordinates, y_coordinates, p=0, q=1)
+
+    x_center = m_10 / m_00
+    y_center = m_01 / m_00
+
+    return x_center, y_center
+
+
+def calc_radial_distance(x_coordinates, y_coordinates, x_center, y_center):
+
+    dist = np.sqrt(np.power(x_coordinates - x_center, 2) + np.power(y_coordinates - y_center, 2))
+
+    return dist
+
+
+def calc_normalized_radial_distance(dist):
+
+    """
+    equation: x / max
+    """
+
+    normalized_radial_distance = dist / np.max(dist)
+
+    return normalized_radial_distance
+
+
+def discrete_fourier_transform(normalized_radial_distance):
+
+    N = normalized_radial_distance.size
+    output = []
+    for u_param in range(N):
+
+        exponential = np.exp((-1j * 2 * np.pi * np.arange(N) * u_param) / N)
+        multiply = normalized_radial_distance * exponential
+
+        output.append(np.average(multiply))
+
+    return np.std(output)
+
+
+def calc_fourier_descriptor(bacteria, fig_export_path, fig_name, img_dimension=[1200, 1200], skip=1):
+    # https://en.wikipedia.org/wiki/Discrete_Fourier_transform
+    """
+    @param  fig_export_path directory to export images
+    @param fig_name image name
+    @return fourier transform
+    """
+
+    contours = image_processing.fill_contours(bacteria, img_dimension=None, um_pixel_ratio=0.144, margin=1)
+    # save image
+    image_processing.save_fig(contours, fig_export_path, fig_name)
+    # external image
+    boundary = image_processing.find_external_contours(fig_export_path + fig_name + '.png', fig_export_path,
+                                                       fig_name + '_boundary')
+    # convert pixel to coordinate
+    x_coordinates, y_coordinates = image_processing.pixel_to_coordinate(boundary)
+    x_coordinates, y_coordinates = image_processing.coordinate_sampling(x_coordinates, y_coordinates, skip=skip)
+    # normalization
+    # x_coordinates = BacteriaFeatures.min_max_normalization(x_coordinates)
+    # y_coordinates = BacteriaFeatures.min_max_normalization(y_coordinates)
+    # center position
+    x_center, y_center = center_coordinate(x_coordinates, y_coordinates)
+    fig, ax = plt.subplots()
+    plt.scatter(x_coordinates, y_coordinates)
+    plt.scatter(x_center, y_center)
+    plt.savefig(fig_export_path + fig_name + '_coordinates.png')
+    radial_distance = calc_radial_distance(x_coordinates, y_coordinates, x_center, y_center)
+    normalized_radial_distance = calc_normalized_radial_distance(radial_distance)
+
+    return discrete_fourier_transform(normalized_radial_distance)
+
