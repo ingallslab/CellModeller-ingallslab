b4304c8fc0f364cb623bfba93f13c8bbd7dda87a
Atiyeh Ahmadi <a94ahmad@uwaterloo.ca>
2023-03-04 09:33:54 -0500

    Unit test is updated

--- Scripts/Run-SS-On-Exp&Sim/unit test/unit_test.py
+++ Scripts/Run-SS-On-Exp&Sim/unit test/unit_test.py
@@ -11,9 +11,11 @@ if __name__ == '__main__':
     input_directory = '../input files/CellModeller_Sample'
 
     summary_statistic_method_list = ["Aspect Ratio", "Anisotropy", "fourier_descriptor"]
+    # Calculation of summary stats on only the last time step
+    only_last_time_step = True
     dt = 0.05  # interval time: 3 min
     # useful for calculation of fractal dimension (optional)
-    fig_export_path = 'fig/sim/'
+    fig_export_path = None
 
     # optional arguments
     # 1. mode: local (calculate for each micro colony) or global(calculate over time lapse without considering
@@ -33,10 +35,13 @@ if __name__ == '__main__':
     # local summary statistics
     # local_mean_summary_statistic_report = data_analysis(input_directory, summary_statistic_method_list, dt,
     #                                                    mode, max_distance_between_cells, um_pixel_ratio,
-    #                                                    min_size_of_micro_colony, fig_path=fig_export_path)
+    #                                                    min_size_of_micro_colony, fig_path=fig_export_path,
+    #                                                          only_last_time_step=only_last_time_step)
     # global summary statistics
     global_mean_summary_statistic_report = data_analysis(input_directory, summary_statistic_method_list, dt, mode,
-                                                         max_distance_between_cells, um_pixel_ratio, fig_path=fig_export_path)
+                                                         max_distance_between_cells, um_pixel_ratio,
+                                                         fig_path=fig_export_path,
+                                                         only_last_time_step=only_last_time_step)
     # mean value of each summary statistic
     # print('local mode:')
     # print(local_mean_summary_statistic_report)
