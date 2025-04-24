3f31426f40d8926087aa59f81f0764c59be89f90
Kyle Pereira <kipereir@uwaterloo.ca>
2025-04-24 12:15:54 -0400

    project: migrate to pyproject, add pre-commit hooks and change doxygen config

--- setup.py
+++ setup.py
@@ -1,37 +1,5 @@
 #!/usr/bin/env python
 
-import os
-import re
 from setuptools import setup
-import subprocess
 
-"""
-23.04.20 add alphashape
-"""
-
-
-setup(name='CellModeller',
-    install_requires=['numpy<2', 'scipy', 'pyopengl', 'mako', 'pyqt5', 'pyopencl', 'reportlab', 'matplotlib',
-                      'vtk',
-                      'pyabc',
-                      'opencv-python',
-                      'scikit-image',
-                      'alphashape'],
-    setup_requires=['numpy<2', 'scipy', 'pyopengl', 'mako', 'pyqt5', 'pyopencl', 'reportlab', 'matplotlib',
-                    'vtk',
-                    'pyabc',
-                    'opencv-python',
-                    'scikit-image',
-                    'alphashape'],
-    packages=['CellModeller',
-                'CellModeller.Biophysics',
-                'CellModeller.Biophysics.BacterialModels',
-                'CellModeller.Biophysics.GeneralModels',
-                'CellModeller.Integration',
-                'CellModeller.Regulation',
-                'CellModeller.Signalling',
-                'CellModeller.GUI'],
-     package_data={'':['*.cl','*.ui']},
-     python_requires='>=3',
-     version="0.0.1"
-)
+setup()
