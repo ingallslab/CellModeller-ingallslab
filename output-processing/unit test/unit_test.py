d3407ee48b60caa5657755030758f9ef7c5bb383
Atiyeh Ahmadi <a94ahmad@uwaterloo.ca>
2025-06-29 18:28:01 +0200

    Update unit_test.py

--- output-processing/unit test/unit_test.py
+++ output-processing/unit test/unit_test.py
@@ -1,13 +1,26 @@
 import sys
+
 sys.path.append('../')
 import CellModellerProcessing
 
-# Getting the location of simulations.
-Input = "../input/cell modeller data"
-# The name of the bacteria Types.
-CellTypes = ['YFP']
-# The location of the CSV output files
-Output = "../output/"
+# Path to the directory containing CellModeller `.pickle` files
+input_dir = "../input/cell modeller data"
+
+# If True, infer and assign cell types to tracked bacteria.
+assign_cell_type = True
+
+# Dictionary mapping cell type names to CellModeller IDs.
+cell_type_mapping = {'YFP': 1}
+
+# If True, approximates the parent bacterium by selecting the nearest disappeared bacterium from
+# the previous time step. Useful when large time step gaps cause the actual parent to no longer appear in
+# the previous step.
+use_grandmother_as_parent = False
+
+# Path to save output CSV files.
+output_dir = "../output/"
 
 # Start Processing
-CellModellerProcessing.starting_process(Input, CellTypes, Output)
+CellModellerProcessing.process_simulation_directory(input_directory=input_dir, cell_type_mapping=cell_type_mapping,
+                                                    output_directory=output_dir, assign_cell_type=assign_cell_type,
+                                                    use_grandmother_as_parent=use_grandmother_as_parent)
