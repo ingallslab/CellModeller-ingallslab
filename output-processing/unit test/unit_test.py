0459016753e1ce6a10131e08ba8b47f810c44619
Atiyeh Ahmadi <a94ahmad@uwaterloo.ca>
2025-06-29 18:34:45 +0200

    Update unit_test.py

--- output-processing/unit test/unit_test.py
+++ output-processing/unit test/unit_test.py
@@ -10,12 +10,17 @@ input_dir = "../input/cell modeller data"
 assign_cell_type = True
 
 # Dictionary mapping cell type names to CellModeller IDs.
-cell_type_mapping = {'YFP': 1}
+cell_type_mapping = {'YFP': 0}
 
 # If True, approximates the parent bacterium by selecting the nearest disappeared bacterium from
 # the previous time step. Useful when large time step gaps cause the actual parent to no longer appear in
 # the previous step.
 use_grandmother_as_parent = False
+
+# Computes neighbor relationships between bacteria based on spatial proximity.
+# Two bacteria are considered neighbors if their expanded pixel boundaries touch.
+# This is consistent with CellProfiler's "MeasureObjectNeighbors" module.
+find_neighbors = True
 
 # Path to save output CSV files.
 output_dir = "../output/"
@@ -23,4 +28,5 @@ output_dir = "../output/"
 # Start Processing
 CellModellerProcessing.process_simulation_directory(input_directory=input_dir, cell_type_mapping=cell_type_mapping,
                                                     output_directory=output_dir, assign_cell_type=assign_cell_type,
-                                                    use_grandmother_as_parent=use_grandmother_as_parent)
+                                                    use_grandmother_as_parent=use_grandmother_as_parent,
+                                                    find_neighbors=find_neighbors)
